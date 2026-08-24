dc9b3d75b80b228555d5db939a87a6174a3b0174
JorgeLMF <63150944+JorgeLMF@users.noreply.github.com>
2026-08-24 13:21:35 -0500

    Fix publicPath for custom domain root

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production' ? '/El_patio_de_Ani/' : '/',
+  publicPath: '/',
 
   chainWebpack: (config) => {
     config.plugin('html').tap((args) => {
